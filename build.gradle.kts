61290a1a1c7466d98ea052f47f5c2201823b1e3b
Denys <denys.shl@mail.com>
2024-04-22 19:29:20 +0300

    hw is done

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.21"
     application
     id("io.gitlab.arturbosch.detekt") version "1.23.3"
 }
@@ -20,7 +20,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 
 application {
